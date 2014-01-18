2266bd9751d29bec50271478bfbcb0b0a3247d93
getsometoast <james.lewis@7digital.com>
2014-01-18 14:37:31 +0000

    intial commit

---
+++ chapter-1/chapter1.clj
@@ -0,0 +1,37 @@
+(first [[['hotdogs]] ['and] ['pickle] relish])
+
+(first (first [[['hotdogs]] ['and] ['pickle] relish]))
+
+(rest ['a 'b 'c])
+
+(rest [['a 'b 'c] 'x 'y 'z])
+
+(rest ['hamburger])
+
+(rest [['x] 't 'r])
+
+(rest 'hotdogs)
+
+(rest [])
+
+(first (rest [['b] ['x 'y] [['c]]]))
+
+(rest (rest [['b] ['x 'y] [['c]]]))
+
+(rest (first ['a ['b ['c]] 'd]))
+
+
+(cons 'peanut ['butter 'and 'jelly])
+
+(cons ['banna 'and] ['peanut 'butter 'and 'jelly])
+
+(cons [['help] 'this] ['is 'very [['hard] 'to 'learn]])
+
+(cons ['a 'b ['c]] [])
+
+(cons 'a [])
+
+(cons [['a 'b 'c]] 'b)
+
+(cons 'a 'b)
+
